3cafc49c46ab70ed8bccbd2dde989e423f216137
Saurabh JJ <saurabhjj@Saurabhs-MacBook-Pro.local>
2021-05-20 12:22:54 +0530

    Added for unique id:

--- books/views.py
+++ books/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+import json
 
+bookData = open('/Users/saurabhjj/POC/python/bookstore/books/resources/books.json').read()
+
+data = json.loads(bookData)
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello books app')
+    context = {'books':data}
+    return render (request,'books/index.html',context)
+
+# Create your views here.
+def show(request,id):
+    context = {'books':data}
+    return render (request,'books/show.html',context)
